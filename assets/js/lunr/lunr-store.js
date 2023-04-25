ed9b4240c11201bd698e821d21b2c708d319a485
pharath <phrth2@gmail.com>
2023-04-25 14:45:57 +0200

    show the excerpt in the search results (instead of the post contents)

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -29,16 +29,7 @@ var store = [
               replace:"</h6>", " "|
             strip_html | strip_newlines | jsonify }},
           {%- else -%}
-            {{ doc.content | newline_to_br |
-              replace:"<br />", " " |
-              replace:"</p>", " " |
-              replace:"</h1>", " " |
-              replace:"</h2>", " " |
-              replace:"</h3>", " " |
-              replace:"</h4>", " " |
-              replace:"</h5>", " " |
-              replace:"</h6>", " "|
-            strip_html | strip_newlines | truncatewords: 50 | jsonify }},
+            {{ doc.excerpt | markdownify | strip_html | truncate: 160 | jsonify }},
           {%- endif -%}
         "categories": {{ doc.categories | jsonify }},
         "tags": {{ doc.tags | jsonify }},
